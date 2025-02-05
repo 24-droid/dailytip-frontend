6fc096746d50476d28781b538209bfc1a0fbd14d
Om Singh <omsingh06102005@gmail.com>
2025-02-05 23:22:55 +0530

    Final changes in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,12 +10,12 @@ const Hero = () => {
       <div className='relative min-h-screen z-50 bg-white' style={{ paddingTop: '5%' }}>
         <ParticleBackground />
         <div className=' relative flex flex-col items-center justify-center'>
-          <h1 className='font-bold text-center' style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Hi, I'm Om.</h1>
-          <h1 className='text-center' style={{ fontSize: '2.1rem', lineHeight: '1.2', fontFamily: 'Roboto', fontWeight: 'bold', marginTop: '1rem' }}>Subscribe to get the tips to build the web better.</h1>
+          <h1 className='font-bold text-center text-black' style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Hi, I'm Om.</h1>
+          <h1 className='text-center text-black' style={{ fontSize: '2.1rem', lineHeight: '1.2', fontFamily: 'Roboto', fontWeight: 'bold', marginTop: '1rem' }}>Subscribe to get the tips to build the web better.</h1>
           <div>
             <div className='flex flex-col  gap-1' style={{ fontSize: '1.1rem', marginTop: '0.8rem' }}>
               <h1 className='text-red-500 text-center text-balance' style={{ fontWeight: 'bold' }}>Do u too hate the complexity of modern web development?</h1>
-              <p className='text-center text-balance'>So here we go we daily provide tips to stay updated with the modern web development, Become more resilient.</p>
+              <p className='text-center text-balance text-black'>So here we go we daily provide tips to stay updated with the modern web development, Become more resilient.</p>
             </div>
             <div>
               <p className='text-center text-black' style={{ marginTop: '2rem',fontSize:'1.2rem',fontWeight:'bold' }}>Join us 👇</p>
@@ -25,7 +25,7 @@ const Hero = () => {
             <button className='h-12 w-46 text-white p-4 rounded-sm hover:cursor-pointer' style={{ backgroundColor: '#F7272F', color: 'white', border: '2px solid black', margin: 'auto' }} onClick={()=>{navigate('/login')}}>Get Daily Developer Tips</button>
           </div>
           <div className='border-b-2 border-gray-500' style={{ borderBottom: '2rem', borderColor: 'gray' }}>
-            <em style={{ fontStyle: 'italic', paddingLeft: '2rem', marginTop: '1rem' }} className='flex flex-wrap md:text-balance md:w-180'>The world is full of innovations, but the world of web development is still in its infancy. We are here to help you stay updated with the latest trends and developments in the field.</em>
+            <em style={{ fontStyle: 'italic', paddingLeft: '2rem', marginTop: '1rem' }} className='flex flex-wrap md:text-balance md:w-180 text-black'>The world is full of innovations, but the world of web development is still in its infancy. We are here to help you stay updated with the latest trends and developments in the field.</em>
             <p style={{ fontStyle: 'italic', paddingLeft: '2rem' }}>-Om</p>
           </div>
           </div>
